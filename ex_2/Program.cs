7a23c7e1c6a2f9f936b5d97620bda4841c85ed8b
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-14 11:39:49 +0600

    add ex_2

---
+++ ex_2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int max =a;
+
+if(max >= b && max >= c)
+{
+    Console.WriteLine($"max number: {max}");
+
+}
+else if(b>= max && b>= c)
+{
+    max = b;
+    Console.WriteLine($"max number: {max}");
+
+}
+else
+{
+    max = c;
+    Console.WriteLine($"max number: {max}");
+   
+}
+    
+
