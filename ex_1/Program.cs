6be2466b171fb4f6cad12931b5f20d6c33e293f8
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-14 11:07:40 +0600

    hw ex_1

---
+++ ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if(a > b)
+{
+    max=a;
+    min=b;
+    Console.WriteLine($"max number: {max}");
+    Console.WriteLine($"min number: {min}");
+}
+else
+{
+    max=b;
+    min=a;
+    Console.WriteLine($"max number: {max}");
+    Console.WriteLine($"min number: {min}");
+}
