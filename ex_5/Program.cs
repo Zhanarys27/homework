1c701ef54b859207fe02b2797e2730848f824252
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-05-22 19:38:27 +0600

    -a

---
+++ ex_5/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num /100 > 0)
+{
+    int i = num;
+    while (i > 1000)
+    {
+        i/=10;
+    }
+
+    
+    int result = i%10;
+    System.Console.WriteLine(result);
+
+    
+}
+else
+System.Console.WriteLine("no");
